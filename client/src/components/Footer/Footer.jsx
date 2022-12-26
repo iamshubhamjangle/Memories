bb5c571f4702c38149fd7a1ecf5a32ad67994448
Shubham Jangle <jangleshubham13@gmail.com>
2022-12-26 23:42:41 +0530

    styling changes

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function Footer() {
+  return (
+    <div id="gradient">
+      <span id="footer-text">
+        Made with <i class="fa-solid fa-heart fa-xl" /> by Shubham Jangle
+      </span>
+    </div>
+  );
+}
+
+export default Footer;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Form from "../Form/Form";
 import Posts from "../Posts/Posts.jsx";
+import Footer from "../Footer/footer";
 
 function Home() {
   return (
     <>
       <Form />
       <Posts />
+      <Footer />
     </>
   );
 }
